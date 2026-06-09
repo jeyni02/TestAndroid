60008e1b8c74784fd500c24aa5668dd5b5b78379
jeyni02 <jeynilomary@uam.edu.ni>
2026-06-09 16:52:35 -0600

    feat: implementar gestor de tareas

--- app/src/main/java/ni/edu/uam/gestiontareas/data/GestorTareas.kt
+++ app/src/main/java/ni/edu/uam/gestiontareas/data/GestorTareas.kt
@@ -1,4 +1,32 @@
 package ni.edu.uam.gestiontareas.data
 
+import ni.edu.uam.gestiontareas.model.Tarea
+
 class GestorTareas {
+
+    private val tareas = mutableListOf<Tarea>()
+
+    fun agregarTarea(tarea: Tarea) {
+        tareas.add(tarea)
+    }
+
+    fun eliminarTarea(id: Int) {
+        tareas.removeIf { it.id == id }
+    }
+
+    fun completarTarea(id: Int) {
+        tareas.find { it.id == id }?.completada = true
+    }
+
+    fun obtenerPendientes(): List<Tarea> {
+        return tareas.filter { !it.completada }
+    }
+
+    fun contarPendientes(): Int {
+        return tareas.count { !it.completada }
+    }
+
+    fun obtenerTodas(): List<Tarea> {
+        return tareas
+    }
 }
